699e20be7f387a656d1a72abd48dde5cc00f47d3
Peio Murguia <peiomurguia@gmail.com>
2018-10-17 12:57:38 +0200

    Commit

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        public static void CreaLista(List <string> compra)
+        {
+            compra.Add("Manzanas");
+            compra.Add("Peras");
+            compra.Add("Leche");
+            compra.Add("Cereales");
+        }
+
+        public static void MostrarLista(List<string> compra)
+        {
+            foreach (string x in compra)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        public static void OrdenarLista(List<string> compra)
+        {
+            compra.Sort();
+            foreach (string x in compra)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        public static void BorrarDeLaLista(List<string> compra, string articulo)
+        {
+            compra.Remove(articulo);
+        }
+
+        public static int ContarElementos(List<string> compra)
+        {
+            
+            return compra.Count;
+        }
+        static void Main(string[] args)
+        {
+            
+            List<string> compra = new List<string>();
+            CreaLista(compra);
+            Console.WriteLine();
+            MostrarLista(compra);
+            Console.WriteLine();
+            OrdenarLista(compra);
+            Console.WriteLine();
+            string articulo = "Manzanas";
+            BorrarDeLaLista(compra,articulo);
+            Console.WriteLine();
+            Console.WriteLine(ContarElementos(compra));
+
+
+            Console.ReadLine();
+        }
+    }
+}
